ba451a50bb0caae8e8c46a192b1defda7e69a589
chido <chaosdomok.21@gmail.com>
2021-04-28 08:01:59 +0200

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 require('dotenv').config()
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
-const prefix = '-';
 
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
