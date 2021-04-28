0c518b6620eb8592e2cc963a4544eb4292fe65bd
chido <chaosdomok.21@gmail.com>
2021-04-28 09:43:25 +0200

    first commit

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,5 +1,15 @@
+const fetch = require("node-fetch").default
 require('dotenv').config()
 module.exports = (Discord, client, message) => {
+    if (message.author.bot || message.channel.type === "dm") return
+    if (message.channel.id === "835214263143497752") {
+        fetch(`https://api.monkedev.com/fun/chat?msg=${message.content}&uid=${message.author.id}&key=0sJZn9q2vhOz4pETsEzWtDsCtUXaCPsK`)
+            .then(response => response.json())
+            .then(data => {
+                message.channel.send(data.response)
+            })
+    }
+
     const prefix = process.env.PREFIX
     if (!message.content.startsWith(prefix) || message.author.bot) return
 
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 require('dotenv').config()
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
+const fetch = require("node-fetch").default
 
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
