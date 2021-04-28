d33e58e1c2e4ec13700274abfb44d32489645400
chido <chaosdomok.21@gmail.com>
2021-04-28 07:58:15 +0200

    first commit

--- counters/member-counter.js
+++
@@ -1,9 +0,0 @@
-module.exports = async(client) => {
-    const guild = client.guilds.cache.get('835214262322331721')
-    setInterval(() => {
-        const memberCount = guild.memberCount
-        const channel = guild.channels.cache.get('836262559127437412')
-        channel.setName(`Użytkownicy: ${memberCount.toLocaleString()}`)
-        console.log('Aktualizowanie ilości użytkowników')
-    }, 5000)
-}
--- index.js
+++ index.js
@@ -1,43 +1,13 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
+require('dotenv').config()
+const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
+const prefix = '-';
 
-const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] })
+client.commands = new Discord.Collection();
+client.events = new Discord.Collection();
 
-const prefix = '-'
-
-const fs = require('fs')
-const { cpuUsage } = require('process')
-
-const memberCounter = require('./counters/member-counter')
-
-client.commands = new Discord.Collection()
-
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`)
-
-    client.commands.set(command.name, command)
-}
-
-client.on('ready', () => {
-    console.log('AnimeFunBot jest online!')
-    memberCounter(client)
+['command_handler', 'event_handler'].forEach(handler => {
+    require(`./handlers/${handler}`)(client, Discord);
 })
 
-client.on('message', message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-    const args = message.content.slice(prefix.length).split(/ +/)
-    const command = args.shift().toLowerCase()
-
-    if (command === 'ping') {
-        client.commands.get('ping').execute(message, args)
-    }
-    if (command === 'clear') {
-        client.commands.get('clear').execute(message, args)
-    }
-    if (command === 'kick') {
-        client.commands.get('kick').execute(message, args)
-    }
-})
-
-client.login('NzQ1NTE5Mjk4MzQ0Mzg2NjAy.Xzy88w.BKepDgKhrLggoN7ZiqZDBrSpHDc')
+client.login(process.env.DISCORD_TOKEN);
