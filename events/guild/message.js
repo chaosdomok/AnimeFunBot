5f1f2b1bc73bb1c774ae6fc0ede95e8868259fea
chido <chaosdomok.21@gmail.com>
2021-04-29 07:10:13 +0200

    first commit

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,22 +1,15 @@
 const fetch = require("node-fetch").default
 require('dotenv').config()
 module.exports = (Discord, client, message) => {
-    if (message.author.bot || message.channel.type === "dm") return
-    if (message.channel.id === "835214263143497752") {
-        fetch(`https://api.monkedev.com/fun/chat?msg=${message.content}&uid=${message.author.id}`)
-            .then(response => response.json())
-            .then(data => {
-                message.channel.send(data.response)
-            })
-            .catch(() => {
-                message.channel.send('Nie udało się pobrać odpowiedzi')
-            })
-    }
 
     const prefix = process.env.PREFIX
     if (!message.content.startsWith(prefix) || message.author.bot) return
 
     const args = message.content.slice(prefix.length).split(/ +/)
+    fetch(`http://clerverbot.luxbot.ml:44236/${args.join('')[1]}`)
+        .then(response => {
+            console.log(response.opd);
+        });
     const cmd = args.shift().toLowerCase()
 
     const command = client.commands.get(cmd)
