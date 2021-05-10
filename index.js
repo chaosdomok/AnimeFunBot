1f0af4b708c488925dad73adcc551c2568c5dcc2
Matheew <82815053+Matheew05@users.noreply.github.com>
2021-05-10 18:51:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
 const Discord = require('discord.js');
 require('dotenv').config()
-const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
-const fetch = require("node-fetch").default
+const client = new Discord.Client();
+const fetch = require("node-fetch");
 
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 
 ['command_handler', 'event_handler'].forEach(handler => {
     require(`./handlers/${handler}`)(client, Discord);
-})
-
-client.on('guildMemberADD', guildMember => {
-    let wlecomeRole = guildMember.guild.roles.cache.find(role => role.name === '(🛑) Nie Zweryfikowany')
-
-    guildMember.roles.add(wlecomeRole)
-    guildMember.guild.channels.cache.get('835214262720004186').send(`Hej <@${guildMember.user.id}> witaj na **AnimeFun**`)
-})
-
-client.on('guildMemberRemove', guildMember => {
-    guildMember.guild.channels.cache.get('835427841008074783').send(`Żegnaj <@${guildMember.user.id}> mamy nadzieje że jescze tu wrócisz`)
-})
+});
 
 client.login(process.env.DISCORD_TOKEN);
