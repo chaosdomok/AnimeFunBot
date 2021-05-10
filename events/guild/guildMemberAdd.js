b2266e91df4d94b54344ed29b9b89447c1219e31
Matheew <82815053+Matheew05@users.noreply.github.com>
2021-05-10 20:20:23 +0200

    Update guildMemberAdd.js

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,5 +1,5 @@
 const {MessageEmbed} = require('discord.js');
 
 module.exports = (client, guild, member) = {
-  client.channels.cache.get('835214262720004186').send(new MessageEmbed().setColor(`GREEN`).setTitle('Witamy!').setDescription(`Witaj ${member} na serwerze ${guild.name}! Baw się dobrze!)
+  if (!member.user.bot) return client.channels.cache.get('835214262720004186').send(new MessageEmbed().setColor(`GREEN`).setTitle('Witamy!').setDescription(`Witaj ${member} na serwerze ${guild.name}! Baw się dobrze!`));
 }
